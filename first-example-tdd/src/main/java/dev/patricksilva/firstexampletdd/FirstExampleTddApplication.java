f4555aedec7ebd3853cde7abf8d6fb88c317aad7
patricksilva1 <patricksilva5160@gmail.com>
2022-11-18 20:33:56 -0300

    example

---
+++ first-example-tdd/src/main/java/dev/patricksilva/firstexampletdd/FirstExampleTddApplication.java
@@ -0,0 +1,13 @@
+package dev.patricksilva.firstexampletdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstExampleTddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstExampleTddApplication.class, args);
+	}
+
+}
---
+++ first-example-tdd/src/test/java/dev/patricksilva/firstexampletdd/FirstExampleTddApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.patricksilva.firstexampletdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstExampleTddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
