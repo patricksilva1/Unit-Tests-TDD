1e1c2cfa723144903128b2c093b4b6a0413d4615
patricksilva1 <patricksilva5160@gmail.com>
2022-11-18 21:29:25 -0300

    Again, Refactor - refactor the code and repeat this cycle.

--- first-example-tdd/src/main/java/dev/patricksilva/firstexampletdd/model/SalesComission.java
+++ first-example-tdd/src/main/java/dev/patricksilva/firstexampletdd/model/SalesComission.java
@@ -2,7 +2,7 @@ package dev.patricksilva.firstexampletdd.model;
 
 public class SalesComission {
 
-    // Simple Example
+    // Simple Example to test
     public double calculate(double saleValue) {
         return (saleValue < 1000.0) ? saleValue * 0.1 : saleValue * 0.15;
     }
